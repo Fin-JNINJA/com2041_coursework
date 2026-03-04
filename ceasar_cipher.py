1f1a3bb9a62612c8eecfc62a6d58e7ad92b8e30b
Fin-JNINJA <finlay.jolley@gmail.com>
2026-03-04 01:28:46 +0000

    question 1 complete

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -1,16 +1,21 @@
 import string
-ciphertext = ""
-plaintext = ""
+ciphertext = "Sokad bgaz vxkbktox wak makxox."
 
-string.ascii_lowercase
-for cipher_letter in ciphertext:
-    upper = False
-    if cipher_letter in string.ascii_letters:
-        if cipher_letter in string.ascii_uppercase:
-            cipher_letter = cipher_letter.lower
-            upper = True
-        # do conversion and mod here
-        if upper:
-            # see if i can remove str once conversion is done
-            cipher_letter = str(cipher_letter).lower
-    plaintext += str(cipher_letter)
+a_len = len(string.ascii_lowercase)
+for i in range(a_len):
+    plaintext = ""
+    for cipher_letter in ciphertext:
+        upper = False
+        if cipher_letter in string.ascii_letters:
+            if cipher_letter in string.ascii_uppercase:
+                cipher_letter = cipher_letter.lower()
+                upper = True
+            # do conversion and mod here
+            cipher_letter = string.ascii_lowercase[(
+                string.ascii_lowercase.index(cipher_letter) + i
+            ) % (a_len)]
+            if upper:
+                # see if i can remove str once conversion is done
+                cipher_letter = cipher_letter.upper()
+        plaintext += cipher_letter
+    print(plaintext)
