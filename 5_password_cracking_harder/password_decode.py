3a146654eb108ace3f420905d7198407df7c86d3
Fin-JNINJA <finlay.jolley@gmail.com>
2026-03-06 18:14:28 +0000

    recursive_rainbow_generation

--- 5_password_cracking_harder/password_decode.py
+++ 5_password_cracking_harder/password_decode.py
@@ -5,20 +5,50 @@ f = open("src/dog_names.txt", "r")
 names = f.read()
 f.close()
 names = names.split("\n")
+#print(names)
+
+def branch(name:str, i:int, symbols:list) ->str:
+    newNames = []
+    newNames.append(name)
+    for symbol in symbols:
+        name = list(name)
+        name[i] = symbol
+        print(str(name))
+        if len(name) < i+1:
+            newNames += substitute(str(name),i+1)
+    return newNames
 
 
-i = 0
+def substitute(name:str,i:int) -> list:
+    
+    newNames = []
+    match list(name)[i].lower():
+        case "o":
+            newNames += branch(name,i,["0","*"])
+        case "i":
+            newNames += branch(name,i,["1","!"])
+        case "a":
+            newNames += branch(name,i,["4","@","&"])
+        case "e":
+            newNames += branch(name,i,["3"])
+        case "s":
+            newNames += branch(name,i,["$","5"])
+        case _:
+            if len(name) < i+1:
+                newNames += substitute(name,i) # i think#newNames += name
+    return newNames
 
+
+extraNames = []
 for name in names:
     #add extra words to check with the formatting extras specified in pdf
-    
-    pass
+    #print(name)
+    extraNames.append(name)
+    extraNames += substitute(name,0)
 
-for name in names:
-    name = name.removeprefix(" ")
-    name = name.removesuffix(" ")
-    name = name.removeprefix("\n")
-    name = name.removesuffix("\n")
+print(extraNames)
+i = 0
+for name in extraNames:
     i += 1
     hash =hashlib.sha256()
     hash.update((name + salt).encode())
@@ -26,5 +56,6 @@ for name in names:
         print(name)
         print(i)
         break
-    if i % 1000 == 0:
+    if i % 50 == 0:
         print(str(i))
+print(i)
