b19a063e459c54c3d82a08468f0f6adba36180d9
Fin-JNINJA <finlay.jolley@gmail.com>
2026-03-17 16:00:57 +0000

    completed_6_decoding_instead

--- 6_cracking_ciphertext_partial_key/partial_key.py
+++ 6_cracking_ciphertext_partial_key/partial_key.py
@@ -9,15 +9,17 @@ for a in string.ascii_lowercase:
             for d in string.ascii_lowercase:
                 sequences.append(a+b+c+d)
 
-f = open("src/11-letter-words.txt", "r")
-letter11 = f.read()
-f.close()
 
-letter_i = []
-for word in letter11:
-    if word[0] == "i":
-        letter_i.append(word)
+#CORRECT AFTER DECRYPT
+letter_filtered = []
+with open("src/11-letter-words.txt") as file:
+    for word in file:
+        if list(word)[0] == "u" and list(word)[2] == "t" and list(word)[3] == "r":
+            print(word)
+            word_san = word.replace("\n","")
+            letter_filtered.append(word_san)
 
+print(letter_filtered)
 print("1")
 output_p = []
 output_s = []
@@ -30,8 +32,9 @@ for sequence in sequences:
             if cipher_letter in string.ascii_uppercase:
                 cipher_letter = cipher_letter.lower()
                 upper = True
-            cipher_letter = string.ascii_lowercase[(
-                string.ascii_lowercase.index(cipher_letter) + string.ascii_lowercase.index(current[i])
+            #MAKE DECRYPT SHOULD BE NOW
+            cipher_letter = string.ascii_lowercase[(26+
+                string.ascii_lowercase.index(cipher_letter) - string.ascii_lowercase.index(current[i])
             ) % (len(string.ascii_lowercase))]
             if upper:
                 cipher_letter = cipher_letter.upper()
@@ -51,8 +54,11 @@ o.close()
 i=0
 for line in output_p:
     i += 1
-    for word in letter_i:
+    for word in letter_filtered:
         if word == line:
             print(str(i)+": "+word)
-    if i % 100 == 1:
+    if i % 10000 == 1:
         print(i)
+
+#Gives: 88320: ultramarine
+#Sequence: faqx
