b34830e1b85630e4cc019263b99e8c1af4cf7209
Fin-JNINJA <finlay.jolley@gmail.com>
2026-03-18 03:28:46 +0000

    using_crib_drag

--- 7_reused_otp/twice_otp_crack.py
+++ 7_reused_otp/twice_otp_crack.py
@@ -1,9 +1,5 @@
 import string
-import itertools
 ciphertexts = ["wxgzgvxpub", "vhwgfblmhq"]
-
-
-# DOES NOT WORK CAUSE ITS 26^10
 
 letter_10_filtered = []
 with open("src/10-letter-words.txt") as file:
@@ -12,48 +8,36 @@ with open("src/10-letter-words.txt") as file:
         letter_10_filtered.append(word_san)
 
 print(letter_10_filtered)
-alphabets = list(itertools.repeat(string.ascii_lowercase, 10))
-print(len(alphabets))
-output_p0 = []
-output_p1 = []
-output_s = []
 
-count = 0
-for a, b, c, d, e, f, g, h, i, j in itertools.product(*alphabets):
-    count += 1
-    if count % 10000 == 1:
-        print(count)
-    current = a+b+c+d+e+f+g+h+i+j
-    # print(current)
-    for ci, ciphertext in enumerate(ciphertexts):
-        plaintext = ""
-        for iter, cipher_letter in enumerate(ciphertext):
-            upper = False
-            if cipher_letter in string.ascii_letters:
-                if cipher_letter in string.ascii_uppercase:
-                    cipher_letter = cipher_letter.lower()
-                    upper = True
-                # MAKE DECRYPT SHOULD BE NOW
-                cipher_letter = string.ascii_lowercase[(26 + string.ascii_lowercase.index(
-                    cipher_letter) - string.ascii_lowercase.index(current[iter])) % (len(string.ascii_lowercase))]
-                if upper:
-                    cipher_letter = cipher_letter.upper()
-            plaintext += cipher_letter
-        # print(sequence + ": " + plaintext)
-        if ci:
-            output_p1.append(plaintext)
-        else:
-            output_p0.append(plaintext)
-    output_s.append(current)
+c0_minus_c1 = ""
 
-o = open("7_reused_otp/output_p0.txt", "w")
-o.write("\n".join(output_p0))
-o.close()
+for i in range(len(ciphertexts[0])):
+    cipher_letter = "?"
+    if ciphertexts[0][i] in string.ascii_letters and ciphertexts[1][i] in string.ascii_letters:
+        cipher_letter = string.ascii_lowercase[(26 +
+                                                string.ascii_lowercase.index(
+                                                    ciphertexts[0][i]) - string.ascii_lowercase.index(ciphertexts[1][i])
+                                                ) % (len(string.ascii_lowercase))]
+    c0_minus_c1 += cipher_letter
+print(c0_minus_c1)
 
-o = open("7_reused_otp/output_p1.txt", "w")
-o.write("\n".join(output_p1))
-o.close()
 
-o = open("7_reused_otp/output_s.txt", "w")
-o.write("\n".join(output_s))
-o.close()
+for word in letter_10_filtered:
+    # c0_minus_c1
+    # print(word)
+    maybe_plain_word = ""
+    for i, letter in enumerate(word):
+        maybe_plain_letter = "?"
+        if letter in string.ascii_letters and c0_minus_c1[i] in string.ascii_letters:
+            maybe_plain_letter = string.ascii_lowercase[(
+                string.ascii_lowercase.index(
+                    letter) + string.ascii_lowercase.index(c0_minus_c1[i])
+            ) % (len(string.ascii_lowercase))]
+        maybe_plain_word += maybe_plain_letter
+    if maybe_plain_word in letter_10_filtered:
+        print(maybe_plain_word, word)
+        break
+
+
+# deciphered
+# cosponsors
